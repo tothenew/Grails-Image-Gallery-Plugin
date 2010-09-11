19baf3cba566e6df2da923938b17d8ebb6ef8abb
Uday Pratap <uday@intelligrape.com>
2010-09-11 16:17:45 +0530

    tested galleria options

--- grails-app/controllers/photogallery/PhotoController.groovy
+++ grails-app/controllers/photogallery/PhotoController.groovy
@@ -2,115 +2,116 @@ package photogallery
 
 class PhotoController {
 
-  static allowedMethods = [save: "POST", update: "POST", delete: "POST"]
+    static allowedMethods = [save: "POST", update: "POST", delete: "POST"]
 
-  def index = {
-    redirect(action: "list", params: params)
-  }
-
-  def list = {
-    List<Photo> photos = []
-    Integer count
-    params.max = Math.min(params.max ? params.int('max') : 10, 100)
-    if (params?.id) {
-      Gallery gallery = Gallery.get(params?.id)
-      photos = gallery.photos as List
-      count = photos?.size()
-    } else {
-      photos = Photo.list(params)
-      count = Photo.count()
+    def index = {
+        redirect(action: "list", params: params)
     }
-    [photos: photos, photoTotal: count]
-  }
 
-  def create = {
-    def photo = new Photo()
-    photo.properties = params
-    return [photo: photo]
-  }
 
-  def save = {PhotoCO photoCO ->
-    def photo = photoCO?.photo
-    if (!photo?.hasErrors() && photo.save()) {
-      flash.message = "Photo has been uploaded"
-      redirect(action: "show", id: photo.id)
-    } else {
-      render(view: "create", model: [photo: photo])
-    }
-  }
-
-  def show = {
-    def photo = Photo.get(params.id)
-    if (!photo) {
-      flash.message = "Photo not found"
-      redirect(action: "list")
-    }
-    else {
-      [photo: photo]
-    }
-  }
-
-  def edit = {
-    def photo = Photo.get(params.id)
-    if (!photo) {
-      flash.message = "Photo not found"
-      redirect(action: "list")
-    }
-    else {
-      return [photo: photo]
-    }
-  }
-
-  def update = {
-    def photo = Photo.get(params.id)
-    if (photo) {
-      if (params.version) {
-        def version = params.version.toLong()
-        if (photo.version > version) {
-
-          photo.errors.rejectValue("version", "Another user has updated this Photo while you were editing","")
-          render(view: "edit", model: [photo: photo])
-          return
+    def list = {
+        List<Photo> photos = []
+        Integer count
+        Gallery gallery = new Gallery()
+        params.max = Math.min(params.max ? params.int('max') : 10, 100)
+        if (params?.id) {
+            gallery = Gallery.get(params?.id)
+            photos = gallery.photos as List
+            count = photos?.size()
+        } else {
+            photos = Photo.list(params)
+            count = Photo.count()
         }
-      }
-      photo.properties = params
-      if (!photo.hasErrors() && photo.save(flush: true)) {
-        flash.message = "Photo has been updated"
-        redirect(action: "show", id: photo.id)
-      }
-      else {
-        render(view: "edit", model: [photo: photo])
-      }
+        [photoInstanceList: photos, photoInstanceTotal: count, gallery: gallery]
     }
-    else {
-      flash.message = "Photo not found"
-      redirect(action: "list")
+    def create = {
+        def photo = new Photo()
+        photo.properties = params
+        return [photo: photo]
     }
-  }
+
+    def save = {PhotoCO photoCO ->
+        def photo = photoCO?.photo
+        if (!photo?.hasErrors() && photo.save()) {
+            flash.message = "Photo has been uploaded"
+            redirect(action: "show", id: photo.id)
+        } else {
+            render(view: "create", model: [photo: photo])
+        }
+    }
+
+    def show = {
+        def photo = Photo.get(params.id)
+        if (!photo) {
+            flash.message = "Photo not found"
+            redirect(action: "list")
+        }
+        else {
+            [photo: photo]
+        }
+    }
+
+    def edit = {
+        def photo = Photo.get(params.id)
+        if (!photo) {
+            flash.message = "Photo not found"
+            redirect(action: "list")
+        }
+        else {
+            return [photo: photo]
+        }
+    }
+
+    def update = {
+        def photo = Photo.get(params.id)
+        if (photo) {
+            if (params.version) {
+                def version = params.version.toLong()
+                if (photo.version > version) {
+
+                    photo.errors.rejectValue("version", "Another user has updated this Photo while you were editing", "")
+                    render(view: "edit", model: [photo: photo])
+                    return
+                }
+            }
+            photo.properties = params
+            if (!photo.hasErrors() && photo.save(flush: true)) {
+                flash.message = "Photo has been updated"
+                redirect(action: "show", id: photo.id)
+            }
+            else {
+                render(view: "edit", model: [photo: photo])
+            }
+        }
+        else {
+            flash.message = "Photo not found"
+            redirect(action: "list")
+        }
+    }
 
 
-  def delete = {
-    def photo = Photo.get(params.id)
-    if (photo) {
-      try {
-        photo.delete(flush: true)
-        flash.message = "Photo has been deleted"
-        redirect(action: "list")
-      }
-      catch (org.springframework.dao.DataIntegrityViolationException e) {
-        flash.message = "Photo could not be deleted"
-        redirect(action: "show", id: params.id)
-      }
+    def delete = {
+        def photo = Photo.get(params.id)
+        if (photo) {
+            try {
+                photo.delete(flush: true)
+                flash.message = "Photo has been deleted"
+                redirect(action: "list")
+            }
+            catch (org.springframework.dao.DataIntegrityViolationException e) {
+                flash.message = "Photo could not be deleted"
+                redirect(action: "show", id: params.id)
+            }
+        }
+        else {
+            flash.message = "Photo could not be found"
+            redirect(action: "list")
+        }
     }
-    else {
-      flash.message = "Photo could not be found"
-      redirect(action: "list")
-    }
-  }
 
-  def showPhoto = {
-    Photo photo = Photo?.get(params?.id)
-    response.setContentType('image/jpg')
-    response.outputStream << (params?.thumbnail ? photo?.thumbnail : photo?.image)
-  }
+    def showPhoto = {
+        Photo photo = Photo?.get(params?.id)
+        response.setContentType('image/jpg')
+        response.outputStream << (params?.thumbnail ? photo?.thumbnail : photo?.image)
+    }
 }
--- grails-app/views/photo/list.gsp
+++ grails-app/views/photo/list.gsp
@@ -1,47 +1,54 @@
 <%@ page import="photogallery.Photo" %>
 <html>
 <head>
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-  <meta name="layout" content="main"/>
-  <title>Photo</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <meta name="layout" content="main"/>
+    <g:javascript library="jquery" plugin="jquery"/>
+    <gallery:resources/>
+    <title>Photo</title>
 </head>
 <body>
 <div class="nav">
-  <span class="menuButton"><a class="home" href="${createLink(uri: '/')}">Home</a></span>
-  <span class="menuButton"><g:link class="create" action="create">New</g:link></span>
+    <span class="menuButton"><a class="home" href="${createLink(uri: '/')}">Home</a></span>
+    <span class="menuButton"><g:link class="create" action="create">New</g:link></span>
 </div>
-<div class="body">
-  <h1>List Photo</h1>
-  <g:if test="${flash.message}">
-    <div class="message">${flash.message}</div>
-  </g:if>
-  <div class="list">
-    <table>
-      <thead>
-      <tr>
-        <g:sortableColumn property="id" title="Id"/>
-        <g:sortableColumn property="thumbnail" title="Image"/>
-        <g:sortableColumn property="caption" title="Caption"/>
-        <g:sortableColumn property="width" title="Width"/>
-        <g:sortableColumn property="height" title="Height"/>
-      </tr>
-      </thead>
-      <tbody>
-      <g:each in="${photos}" status="i" var="photo">
-        <tr class="${(i % 2) == 0 ? 'odd' : 'even'}">
-          <td><g:link action="show" id="${photo.id}">${photo.id}</g:link></td>
-          <td><g:link action="show" id="${photo.id}"><img src="${createLink(action: 'showPhoto', controller: 'photo', id: photo?.id, params: [thumbnail: true])}" width="100" height="100"/></g:link></td>
-          <td><g:link action="show" id="${photo.id}">${photo.caption}</g:link></td>
-          <td><g:link action="show" id="${photo.id}">${photo.width}</g:link></td>
-          <td><g:link action="show" id="${photo.id}">${photo.height}</g:link></td>
-        </tr>
-      </g:each>
-      </tbody>
-    </table>
-  </div>
-  <div class="paginateButtons">
-    <g:paginate total="${photoTotal}"/>
-  </div>
-</div>
+<g:if test="${gallery?.id}">
+    <gallery:show galleryInstance="${gallery}" options="height:200,preload:0,carousel:false, image_pan_smoothness:5"/>
+</g:if>
+<g:else>
+    <div class="body">
+        <h1>List Photo</h1>
+        <g:if test="${flash.message}">
+            <div class="message">${flash.message}</div>
+        </g:if>
+        <div class="list">
+            <table>
+                <thead>
+                <tr>
+                    <g:sortableColumn property="id" title="Id"/>
+                    <g:sortableColumn property="thumbnail" title="Image"/>
+                    <g:sortableColumn property="caption" title="Caption"/>
+                    <g:sortableColumn property="width" title="Width"/>
+                    <g:sortableColumn property="height" title="Height"/>
+                </tr>
+                </thead>
+                <tbody>
+                <g:each in="${photos}" status="i" var="photo">
+                    <tr class="${(i % 2) == 0 ? 'odd' : 'even'}">
+                        <td><g:link action="show" id="${photo.id}">${photo.id}</g:link></td>
+                        <td><g:link action="show" id="${photo.id}"><img src="${createLink(action: 'showPhoto', controller: 'photo', id: photo?.id, params: [thumbnail: true])}" width="100" height="100"/></g:link></td>
+                        <td><g:link action="show" id="${photo.id}">${photo.caption}</g:link></td>
+                        <td><g:link action="show" id="${photo.id}">${photo.width}</g:link></td>
+                        <td><g:link action="show" id="${photo.id}">${photo.height}</g:link></td>
+                    </tr>
+                </g:each>
+                </tbody>
+            </table>
+        </div>
+        <div class="paginateButtons">
+            <g:paginate total="${photoTotal}"/>
+        </div>
+    </div>
+</g:else>
 </body>
 </html>
