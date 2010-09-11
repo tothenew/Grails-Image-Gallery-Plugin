b354cd8cf6d91788ed0b1f7f28e86dac1ff44531
Uday Pratap <uday@intelligrape.com>
2010-09-11 14:36:36 +0530

    added description

--- grails-app/domain/photogallery/Photo.groovy
+++ grails-app/domain/photogallery/Photo.groovy
@@ -3,29 +3,31 @@ package photogallery
 import org.grails.plugins.imagetools.ImageTool
 
 class Photo {
-  byte[] thumbnail
-  String caption
-  byte[] image
-  Integer width
-  Integer height
-  static mapping = {
-  }
-  static constraints = {
-    thumbnail(nullable: true)
-    caption(nullable: true)
-    image(validator: {
-      return it?.size() > 0
-    })
-    width(nullable: false)
-    height(nullable: false)
-  }
-
-  public static void loadThumbnail(Photo photo) {
-    if (photo) {
-      def imageTool = new ImageTool()
-      imageTool.load(photo.image)
-      imageTool.thumbnail(100)   
-      photo.thumbnail = imageTool.getBytes("JPEG")
+    byte[] thumbnail
+    String caption
+    byte[] image
+    Integer width
+    Integer height
+    String description
+    static mapping = {
     }
-  }
+    static constraints = {
+        thumbnail(nullable: true)
+        description(nullable: true)
+        caption(nullable: true)
+        image(validator: {
+            return it?.size() > 0
+        })
+        width(nullable: false)
+        height(nullable: false)
+    }
+
+    public static void loadThumbnail(Photo photo) {
+        if (photo) {
+            def imageTool = new ImageTool()
+            imageTool.load(photo.image)
+            imageTool.thumbnail(100)
+            photo.thumbnail = imageTool.getBytes("JPEG")
+        }
+    }
 }
